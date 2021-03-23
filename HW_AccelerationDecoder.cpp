7bfc8181e1977ace4468517a119d7a36e2575f29
Dimitris Tsiakmakis <dimatsiak@gmail.com>
2021-03-23 17:11:26 +0200

    first approach with hw acceleration / doesn't run the code

--- HW_AccelerationDecoder.cpp
+++ HW_AccelerationDecoder.cpp
@@ -1,86 +1,332 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <fstream>
 #include <sstream>
-
-extern "C"
-{
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libavformat/avio.h"
-#include "libswscale/swscale.h"
-#include "libavutil/imgutils.h"
-#include "SDL.h"
-#include "SDL_image.h"
-}
-
-#include "SDLFunctionality.h"
 #include <wtypes.h>
 #include <chrono>
 #include <thread>
-
 #include <ppltasks.h>
-#include <iostream>
-#include "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/um/libloaderapi.h"
+#include <fstream>
+#include <iterator>
+#include <vector>
 
-using namespace concurrency;
-using namespace std;
+extern "C"
+{
+    #include "libavcodec/avcodec.h"
+    #include "libavformat/avformat.h"
+    #include "libavformat/avio.h"
+    #include "libswscale/swscale.h"
+    #include "libavutil/imgutils.h"
+    #include "SDL.h"
+    #include "SDL_image.h"
+#include <libavutil/file.h>
+}
+
+#include "SDLFunctionality.h"
+
 
 #undef main
 
-//also, check -> https://github.com/leixiaohua1020/simplest_ffmpeg_streamer/blob/master/simplest_ffmpeg_receiver/simplest_ffmpeg_receiver.cpp
+struct buffer_data {
+	uint8_t* ptr;
+	size_t size; ///< size left in the buffer
+};
+struct s_dimension {
+	int width;
+	int height;
+};
+struct s_decoded_frame {
+	uint8_t* data; //data as rgb or yuv format; set the dependencies
+	int linesize;
+	int width;
+	int height;
+};
 
-typedef HRESULT(CALLBACK* LPFNDLLFUNC1)(int, int);
-typedef svideoparams* (CALLBACK* LPFNDLLINIT)(const char*);
-typedef sdecoded_frame* (CALLBACK* LPFNDLLLIVESTREAMING)(void);
-typedef int(CALLBACK* LPFNDLLCLOSE)(void);
+AVBufferRef* hw_device_ctx = NULL;
+enum AVHWDeviceType type;
+
+AVCodec* Codec = NULL;
+AVCodecContext* codec_ctx = NULL;
+AVPacket* pkt = NULL;
+AVFrame* frame = NULL, * pFrameRGB = NULL, * sw_frame = NULL, * tmp_frame = NULL;
+AVFormatContext* fmt_ctx = NULL;
+AVIOContext* avio_ctx = NULL;
+
+struct SwsContext* sws_ctx = NULL;
+
+uint8_t* buffer = NULL, * avio_ctx_buffer = NULL;
+int numBytes = 0;
+uint8_t* buffer_rgb;
+size_t buffer_size, avio_ctx_buffer_size = 1024 * 180;
+int ret = 0;
+struct buffer_data bd = { 0 };
+
+s_decoded_frame* struct_export;
+s_dimension* dimension_export;
+
+s_decoded_frame* decode(AVCodecContext*, AVFrame*, AVPacket*);
+s_decoded_frame* decode_hw_acceleration(AVCodecContext*, AVFrame*, AVPacket*);
 
 
+static enum AVPixelFormat hw_pix_fmt;
 
-int wmain(int argc, char** argv) {
-    printf("START PROGRAM __");
-    SDLFunctionality* sdlf = new SDLFunctionality();
+static int read_packet(void* opaque, uint8_t* buf, int buf_size)
+{
+	struct buffer_data* bd = (struct buffer_data*)opaque;
+	buf_size = FFMIN(buf_size, bd->size);
 
-    HINSTANCE hDLL;               // Handle to DLL
-    LPFNDLLFUNC1 lpfnDllFunc1;    // Function pointer
-    LPFNDLLINIT lpfnDllInit;
-    LPFNDLLLIVESTREAMING lpfnDllLiveStreaming;
-    LPFNDLLCLOSE lpfnDllClose;
+	printf("ptr:%p size:%zu\n", bd->ptr, bd->size);
 
-    HRESULT hrReturnVal;
-    svideoparams* hrReturnInit;
-    sdecoded_frame* hrReturnLiveStreaming;
+	/* copy internal buffer data to buf */
+	memcpy(buf, bd->ptr, buf_size);
+	bd->ptr += buf_size;
+	bd->size -= buf_size;
 
-    hDLL = LoadLibrary(L"RTStreaming.dll");
+	return buf_size;
+}
+static enum AVPixelFormat get_hw_format(AVCodecContext* ctx, const enum AVPixelFormat* pix_fmts)
+{
+	const enum AVPixelFormat* p;
+	for (p = pix_fmts; *p != -1; p++) {
+		if (*p == hw_pix_fmt)
+			return *p;
+	}
 
-    lpfnDllInit = (LPFNDLLINIT)GetProcAddress(hDLL, "init");
-    lpfnDllLiveStreaming = (LPFNDLLLIVESTREAMING)GetProcAddress(hDLL, "live_streaming");
-    lpfnDllClose = (LPFNDLLCLOSE)GetProcAddress(hDLL, "close");
+	fprintf(stderr, "Failed to get HW surface format.\n");
+	return AV_PIX_FMT_NONE;
+}
 
+s_dimension* init_ffmpeg(uint8_t* buffer_in, int buffer_size_in)
+{
+	bd.ptr = buffer_in;
+	bd.size = buffer_size_in;
+	if (!(fmt_ctx = avformat_alloc_context())) {
+		ret = AVERROR(ENOMEM);
+		printf("Error : avformat_alloc_context\n");
+		return NULL;
+	}
+	avio_ctx_buffer = static_cast<uint8_t*>(av_malloc(avio_ctx_buffer_size));
+	if (!avio_ctx_buffer) {
+		ret = AVERROR(ENOMEM);
+		printf("Error : avio_ctx_buffer = av_malloc(avio_ctx_buffer_size)\n");
+		return NULL;
+	}
+	avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size, 0, &bd, &read_packet, NULL, NULL);
+	if (!avio_ctx) {
+		ret = AVERROR(ENOMEM);
+		printf("Error : avio_ctx = avio_alloc_context();\n");
+		return NULL;
+	}
+	fmt_ctx->pb = avio_ctx;
 
-    //hrReturnVal = lpfnDllFunc1(3, 3);
-    const char* filename = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+	ret = avformat_open_input(&fmt_ctx, NULL, NULL, NULL);
+	if (ret < 0) {
+		printf("Could not open input\n");
+		return NULL;
+	}
+	ret = avformat_find_stream_info(fmt_ctx, NULL);
+	if (ret < 0) {
+		printf("Could not find stream information\n");
+		return NULL;
+	}
 
+	codec_ctx = avcodec_alloc_context3(NULL);
 
-    hrReturnInit = lpfnDllInit(filename);
-    std::cout << "value returned : " << hrReturnInit->width << ", " << std::endl;
-    //hrReturnLiveStreaming = lpfnDllLiveStreaming();
-    int i = 0;
-    sdlf->Init();
-    while (true) {
-        i++;
-        sdecoded_frame* frame = lpfnDllLiveStreaming();
-        sdlf->ShowImage(frame);
-        if (i > 180) {
-            break;
-        }
-    }
-    int k = lpfnDllClose();
-    printf("close result : %d\n", k);
-    //sdlf->Quit();
+	if (ret = avcodec_parameters_to_context(codec_ctx, fmt_ctx->streams[0]->codecpar) < 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot get codec parameters\n");
+		return NULL;
+	}
+
+	Codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+
+	if (Codec == NULL)
+	{
+		av_log(NULL, AV_LOG_ERROR, "No decoder found\n");
+		return NULL;
+	}
+
+	if (ret = avcodec_open2(codec_ctx, Codec, NULL) < 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot open video decoder\n");
+		return NULL;
+	}
+	pkt = av_packet_alloc();
+	av_init_packet(pkt);
+	if (!pkt)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot init packet\n");
+		return NULL;
+	}
+
+	frame = av_frame_alloc();
+	if (!frame)
+	{
+		av_log(NULL, AV_LOG_ERROR, "Cannot init frame\n");
+		return NULL;
+	}
+
+	if (ret = av_read_frame(fmt_ctx, pkt) < 0)
+	{
+		av_log(NULL, AV_LOG_ERROR, "cannot read frame");
+		return NULL;
+	}
+
+	struct_export = (s_decoded_frame*)malloc(sizeof(s_decoded_frame));
+	dimension_export = (s_dimension*)malloc(sizeof(s_dimension));
+
+	pFrameRGB = av_frame_alloc();
+	if (pFrameRGB == NULL)
+	{
+		return NULL;
+	}
+	sw_frame = av_frame_alloc();
+	if (sw_frame == NULL)
+	{
+		return NULL;
+	}
+	tmp_frame = av_frame_alloc();
+	if (tmp_frame == NULL)
+	{
+		return NULL;
+	}
+
+	numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 16);
+	buffer_rgb = (uint8_t*)av_malloc(numBytes * sizeof(uint8_t));
+
+	sws_ctx = sws_getContext(codec_ctx->width, codec_ctx->height, codec_ctx->pix_fmt, codec_ctx->width, codec_ctx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+	av_image_fill_arrays(pFrameRGB->data, pFrameRGB->linesize, buffer_rgb, AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1);
+
+	dimension_export->width = codec_ctx->width;
+	dimension_export->height = codec_ctx->height;
+
+	return dimension_export;
+}
+s_dimension* init_ffmpeg_hw_acceleration(uint8_t* buffer_in, int buffer_size_in)
+{
+	s_dimension* dimension_export = (s_dimension*)malloc(sizeof(s_dimension));
+	dimension_export = init_ffmpeg(buffer_in, buffer_size_in);
+	type = av_hwdevice_find_type_by_name("d3d11va");
+	//https://github.com/FFmpeg/FFmpeg/blob/7b100839330ace3b4846ee4a1fc5caf4b8f8a34e/doc/examples/hw_decode.c#L166
+	//show available device type ^
+
+	for (int i = 0;; i++) {
+		const AVCodecHWConfig* config = avcodec_get_hw_config(Codec, i);
+		if (!config) {
+			fprintf(stderr, "Decoder %s does not support device type %s.\n",
+				Codec->name, av_hwdevice_get_type_name(type));
+			return NULL;
+		}
+		if (config->methods & AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX &&
+			config->device_type == type) {
+			hw_pix_fmt = config->pix_fmt;
+			break;
+		}
+	}
+	codec_ctx->get_format = get_hw_format;
+	int err = 0;
+	if ((err = av_hwdevice_ctx_create(&hw_device_ctx, type, NULL, NULL, 0)) < 0) {
+		fprintf(stderr, "Failed to create specified HW device.\n");
+		return NULL;
+	}
+	codec_ctx->hw_device_ctx = av_buffer_ref(hw_device_ctx);
+
+	return dimension_export;
+}
+s_decoded_frame* decode_ffmpeg_hw_acceleration(uint8_t* data, int length)
+{
+	int result = av_packet_from_data(pkt, data, length);
+	if (result < 0) {
+		printf("Error : av_packet_from_data\n");
+		return NULL;
+	}
+
+	s_decoded_frame* struct_export = decode_hw_acceleration(codec_ctx, frame, pkt);
+	return struct_export;
+}
+s_decoded_frame* decode_hw_acceleration(AVCodecContext* dec_ctx, AVFrame* frame, AVPacket* pkt)
+{
+	ret = avcodec_send_packet(dec_ctx, pkt);
+	if (ret < 0) {
+		fprintf(stderr, "Error sending a packet for decoding\n");
+		return NULL;
+	}
+	int index = 0;
+	ret = avcodec_receive_frame(dec_ctx, frame);
+	if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+		printf("AVERROR(EAGAIN) or AVERROR_EOF");
+		return NULL;
+	}
+	else if (ret < 0) {
+		fprintf(stderr, "Error during decoding\n");
+		return NULL;
+	}
+	fflush(stdout);
+
+	if (frame->format == hw_pix_fmt) {
+		/* retrieve data from GPU to CPU */
+		if ((ret = av_hwframe_transfer_data(sw_frame, frame, 0)) < 0) {
+			fprintf(stderr, "Error transferring the data to system memory\n");
+			return NULL;
+		}
+		tmp_frame = sw_frame;
+	}
+	else
+		tmp_frame = frame;
+
+	//Convert the image from its natice format to RGB.
+	sws_scale(sws_ctx, (uint8_t const* const*)tmp_frame->data, tmp_frame->linesize, 0, codec_ctx->height, pFrameRGB->data, pFrameRGB->linesize);
+
+	struct_export->data = pFrameRGB->data[0];
+	struct_export->linesize = pFrameRGB->linesize[0];
+	struct_export->width = codec_ctx->width;
+	struct_export->height = codec_ctx->height;
+
+	return struct_export;
+}
+char* get_filename(int index_name)
+{
+	char s1[5] = "D:\\\\";
+	char s2[7];	//6-pshfios ariumos max(999.999 max frames + 1 NULL_TERMINATOR)
+
+	if (index_name > 9999)
+	{
+		printf("ERROR : index_name > 6 digits number\n");
+		return NULL;
+	}
+	sprintf(s2, "%d", index_name);
+
+	char* result = (char*)malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("START PROGRAM __");
+    SDLFunctionality* sdlf = new SDLFunctionality();
+    int i = 0;
+	s_dimension* dims;
+    sdlf->Init();
+    while (i < 70) {
+        i++;
+		uint8_t* data = NULL;
+		size_t iSize = NULL;
+		s_decoded_frame* struct_extracted = NULL;
+
+		printf("Hello World!\n");
+		av_file_map(get_filename(i), &data, &iSize, NULL, NULL);
+
+		dims = init_ffmpeg_hw_acceleration(data, iSize);
+        //sdecoded_frame* frame = NULL;//lpfnDllLiveStreaming();
+        //sdlf->ShowImage(frame);
+		if (dims == NULL) {
+			printf("dims is null\n");
+		}
+		
+    }
+    //printf("close result : %d\n", 6);
+    //sdlf->Quit();
+
 }
